187052cc4f452f4748ac56e3c1bf933e28b298b0
erikamikami <g.erika3up@gmail.com>
2022-09-02 12:39:18 +0900

    first commit

---
+++ src/main/java/com/example/BatchRetryChunkApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchRetryChunkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchRetryChunkApplication.class, args);
+	}
+
+}
